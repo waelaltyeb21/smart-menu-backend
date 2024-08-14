47d4dcadc09b2f67e4d6d1c92de667b26e5982b5
waelaltyeb21 <waelaltyeb2124@gmail.com>
2024-08-14 03:49:20 -0700

    Dashboard v 1.0

--- Public/index.js
+++ Public/index.js
@@ -9,6 +9,7 @@ const mongo = require("mongoose");
 app.use(
   cors({
     origin: [
+      "https://smart-menu-admin-dashboard.onrender.com",
       "https://smart-menu-client.onrender.com",
       "http://localhost:5173",
       "http://localhost:5174"
